d1686dbfda1b37c284a6795bc8c4006b7d25a2b1
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-12 17:40:29 +0530

    Add files via upload

---
+++ controllers/successController.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+exports.getSuccess = (req,res,next)=>
+{
+    res.sendFile(path.join(__dirname,'../','views','success.html'))    
+}
+
+exports.postSucccess = (req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/success')
+}
